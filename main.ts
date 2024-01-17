d80512ce5bf237c0e25fe7809e7217e9cacc91f0
zhaiii55 <156235611+zhaiii55@users.noreply.github.com>
2024-01-17 13:09:55 -0600

    Make sure you finish granbdma anbd try playing it with two players to check if it works.

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+}
 scene.setBackgroundColor(15)
 tiles.setCurrentTilemap(tilemap`beggining`)
 let spy = sprites.create(assets.image`spy`, SpriteKind.Player)
@@ -29,6 +32,99 @@ let camera_cruz = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(camera_cruz)
+let grandma = sprites.create(assets.image`grandma`, SpriteKind.NPC)
+tiles.placeOnTile(grandma, tiles.getTileLocation(5, 12))
+animation.runImageAnimation(
+grandma,
+[img`
+    . f f f 1 1 1 1 1 1 1 1 1 f f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 b b b b b 1 f . 
+    . f 1 1 b d b b b d d d d 1 f f 
+    f f 1 b d d d d d d d d d b 1 f 
+    f 1 b d d d d d d d d d d b f f 
+    f f b d d f f d d d f f d f f . 
+    . f f d d d d d d d d d d f . . 
+    . f 1 1 c 1 1 1 1 1 5 5 1 f f f 
+    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
+    . f d d c 5 5 5 5 5 5 5 f d e f 
+    . f d d c 5 5 5 5 5 5 f f f e f 
+    . f c 5 c 5 1 5 1 5 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f f f f d d f d d f f . f e f 
+    `,img`
+    . . . . f f f f f f f f f . . . 
+    . f f f 1 1 1 1 1 1 1 1 1 f f . 
+    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
+    . f 1 1 1 1 1 1 b b b b b 1 f . 
+    . f 1 1 b d b b b d d d d 1 f . 
+    f f 1 b d d d d d d d d d b 1 . 
+    f 1 b d d d d d d d d d d b f . 
+    f f b d d f f d d d f f d f f . 
+    f f f d 9 d d d d d d d 9 f f . 
+    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
+    . f d d c 5 5 5 5 5 5 5 f d e f 
+    . f d d c 5 5 5 5 5 5 f f f e f 
+    . f c 5 c 5 1 5 1 5 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f f f f d d f d d f f . f e f 
+    `,img`
+    . c . c f c f f f f f f f . . . 
+    . c f c 1 c 1 1 1 1 1 1 1 f f . 
+    . c 1 c 1 1 1 1 1 1 1 1 1 1 f . 
+    . c 1 1 1 1 1 1 b b b b b 1 f . 
+    . f 1 1 b d b b b d d d d 1 f . 
+    f f 1 b d d d d d d d d d b 1 . 
+    f 1 b d d d d d d d d d d b f . 
+    f f b d d f f d d d f f d f f . 
+    f f f d 9 d d d d d d d 9 f f . 
+    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
+    . f d d c 5 5 5 5 5 5 5 f d e f 
+    . f d d c 5 5 5 5 5 5 f f f e f 
+    . f c 5 c 5 1 5 1 5 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f f f f d d f d d f f . f e f 
+    `,img`
+    . c . c f c f f f f f f f . . . 
+    . c f c 1 c 1 1 1 1 1 1 1 f f . 
+    . c 1 c 1 c 1 1 1 1 1 1 1 1 f . 
+    . c 1 c 1 1 1 1 b b b b b 1 f . 
+    . c 1 1 b d b b b d d d d 1 f . 
+    f f 1 b d d d d d d d d d b 1 . 
+    f 1 b d d d d d d d d d d b f . 
+    f f b d d f f d d d f f d f f . 
+    f f f d 9 d d d d d d d 9 f f . 
+    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
+    . f d d c 5 5 5 5 5 5 5 f d e f 
+    . f d d c 5 5 5 5 5 5 f f f e f 
+    . f c 5 c 5 1 5 1 5 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f f f f d d f d d f f . f e f 
+    `,img`
+    . c . c . c . c . . . . . . . . 
+    . c . c f c f f f f f f f . . . 
+    . c f c 1 c 1 1 1 1 1 1 1 f f . 
+    . c 1 c 1 c 1 1 1 1 1 1 1 1 f . 
+    . c 1 c 1 1 1 1 b b b b b 1 f . 
+    . c 1 1 b d b b b d d d d 1 f . 
+    f f 1 b d d d d d d d d d b 1 . 
+    f 1 b d d d d d d d d d d b f . 
+    f f b d d f f d d d f f d f f . 
+    f f f d 9 d d d d d d d 9 f f f 
+    . f d d c 5 1 1 1 5 5 5 f d e f 
+    . f d d c 5 5 5 5 5 5 f f f e f 
+    . f c 5 c 5 5 5 5 5 1 f . f e f 
+    . f c c c 5 1 5 1 5 1 f . f e f 
+    . f c c c 1 1 1 1 1 1 f . f e f 
+    . f f f f d d f d d f f . f e f 
+    `],
+500,
+true
+)
 game.onUpdate(function () {
     camera_cruz.setPosition((spy.x + spy_buddy.x) / 2, (spy.y + spy_buddy.y) / 2)
 })
