0f162d309f216f48bc06433027407f22c75c130c
zhaiii55 <156235611+zhaiii55@users.noreply.github.com>
2024-01-19 12:56:35 -0600

    Have trouble with making players shoot thier attack and having the sprites dissapear after talking to grandma and also the long text.

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 namespace SpriteKind {
     export const NPC = SpriteKind.create()
+    export const grnadmacloseup = SpriteKind.create()
 }
+let rock: Sprite = null
+let mySprite: Sprite = null
+let pressA: Sprite = null
 scene.setBackgroundColor(15)
 tiles.setCurrentTilemap(tilemap`beggining`)
 let spy = sprites.create(assets.image`spy`, SpriteKind.Player)
@@ -8,10 +12,10 @@ let spy_buddy = sprites.create(assets.image`spy buddy`, SpriteKind.Player)
 tiles.placeOnRandomTile(spy, sprites.castle.tilePath5)
 tiles.placeOnRandomTile(spy_buddy, sprites.castle.tilePath5)
 controller.moveSprite(spy, 100, 100)
-tiles.placeOnTile(spy, tiles.getTileLocation(6, 1))
+tiles.placeOnTile(spy, tiles.getTileLocation(0, 1))
 spy.setStayInScreen(true)
 controller.moveSprite(spy_buddy, 100, 100)
-tiles.placeOnTile(spy_buddy, tiles.getTileLocation(7, 1))
+tiles.placeOnTile(spy_buddy, tiles.getTileLocation(1, 1))
 spy_buddy.setStayInScreen(true)
 let camera_cruz = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -33,98 +37,31 @@ let camera_cruz = sprites.create(img`
     `, SpriteKind.Player)
 scene.cameraFollowSprite(camera_cruz)
 let grandma = sprites.create(assets.image`grandma`, SpriteKind.NPC)
-tiles.placeOnTile(grandma, tiles.getTileLocation(5, 12))
+tiles.placeOnTile(grandma, tiles.getTileLocation(3, 1))
 animation.runImageAnimation(
 grandma,
-[img`
-    . f f f 1 1 1 1 1 1 1 1 1 f f . 
-    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
-    . f 1 1 1 1 1 1 b b b b b 1 f . 
-    . f 1 1 b d b b b d d d d 1 f f 
-    f f 1 b d d d d d d d d d b 1 f 
-    f 1 b d d d d d d d d d d b f f 
-    f f b d d f f d d d f f d f f . 
-    . f f d d d d d d d d d d f . . 
-    . f 1 1 c 1 1 1 1 1 5 5 1 f f f 
-    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
-    . f d d c 5 5 5 5 5 5 5 f d e f 
-    . f d d c 5 5 5 5 5 5 f f f e f 
-    . f c 5 c 5 1 5 1 5 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f f f f d d f d d f f . f e f 
-    `,img`
-    . . . . f f f f f f f f f . . . 
-    . f f f 1 1 1 1 1 1 1 1 1 f f . 
-    . f 1 1 1 1 1 1 1 1 1 1 1 1 f . 
-    . f 1 1 1 1 1 1 b b b b b 1 f . 
-    . f 1 1 b d b b b d d d d 1 f . 
-    f f 1 b d d d d d d d d d b 1 . 
-    f 1 b d d d d d d d d d d b f . 
-    f f b d d f f d d d f f d f f . 
-    f f f d 9 d d d d d d d 9 f f . 
-    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
-    . f d d c 5 5 5 5 5 5 5 f d e f 
-    . f d d c 5 5 5 5 5 5 f f f e f 
-    . f c 5 c 5 1 5 1 5 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f f f f d d f d d f f . f e f 
-    `,img`
-    . c . c f c f f f f f f f . . . 
-    . c f c 1 c 1 1 1 1 1 1 1 f f . 
-    . c 1 c 1 1 1 1 1 1 1 1 1 1 f . 
-    . c 1 1 1 1 1 1 b b b b b 1 f . 
-    . f 1 1 b d b b b d d d d 1 f . 
-    f f 1 b d d d d d d d d d b 1 . 
-    f 1 b d d d d d d d d d d b f . 
-    f f b d d f f d d d f f d f f . 
-    f f f d 9 d d d d d d d 9 f f . 
-    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
-    . f d d c 5 5 5 5 5 5 5 f d e f 
-    . f d d c 5 5 5 5 5 5 f f f e f 
-    . f c 5 c 5 1 5 1 5 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f f f f d d f d d f f . f e f 
-    `,img`
-    . c . c f c f f f f f f f . . . 
-    . c f c 1 c 1 1 1 1 1 1 1 f f . 
-    . c 1 c 1 c 1 1 1 1 1 1 1 1 f . 
-    . c 1 c 1 1 1 1 b b b b b 1 f . 
-    . c 1 1 b d b b b d d d d 1 f . 
-    f f 1 b d d d d d d d d d b 1 . 
-    f 1 b d d d d d d d d d d b f . 
-    f f b d d f f d d d f f d f f . 
-    f f f d 9 d d d d d d d 9 f f . 
-    . f 1 1 c 5 1 1 1 5 5 5 1 d d f 
-    . f d d c 5 5 5 5 5 5 5 f d e f 
-    . f d d c 5 5 5 5 5 5 f f f e f 
-    . f c 5 c 5 1 5 1 5 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f f f f d d f d d f f . f e f 
-    `,img`
-    . c . c . c . c . . . . . . . . 
-    . c . c f c f f f f f f f . . . 
-    . c f c 1 c 1 1 1 1 1 1 1 f f . 
-    . c 1 c 1 c 1 1 1 1 1 1 1 1 f . 
-    . c 1 c 1 1 1 1 b b b b b 1 f . 
-    . c 1 1 b d b b b d d d d 1 f . 
-    f f 1 b d d d d d d d d d b 1 . 
-    f 1 b d d d d d d d d d d b f . 
-    f f b d d f f d d d f f d f f . 
-    f f f d 9 d d d d d d d 9 f f f 
-    . f d d c 5 1 1 1 5 5 5 f d e f 
-    . f d d c 5 5 5 5 5 5 f f f e f 
-    . f c 5 c 5 5 5 5 5 1 f . f e f 
-    . f c c c 5 1 5 1 5 1 f . f e f 
-    . f c c c 1 1 1 1 1 1 f . f e f 
-    . f f f f d d f d d f f . f e f 
-    `],
+assets.animation`grandma cries`,
 500,
 true
 )
 game.onUpdate(function () {
     camera_cruz.setPosition((spy.x + spy_buddy.x) / 2, (spy.y + spy_buddy.y) / 2)
+    if (spy.overlapsWith(grandma) || spy.overlapsWith(grandma)) {
+        pressA = sprites.create(assets.image`pressA`, SpriteKind.Player)
+        pressA.setPosition(75, 100)
+    }
+    if (controller.A.isPressed() && spy.overlapsWith(grandma) || controller.A.isPressed() && spy_buddy.overlapsWith(grandma)) {
+        mySprite = sprites.create(assets.image`Grandma close up 1`, SpriteKind.grnadmacloseup)
+        mySprite.setPosition(50, 60)
+        game.showLongText("My grandsin is missing!", DialogLayout.Bottom)
+        game.showLongText("Help me find my grandson!", DialogLayout.Bottom)
+        game.showLongText("Hes at the end of the maze...", DialogLayout.Bottom)
+        game.showLongText("Please help me...", DialogLayout.Bottom)
+        game.showLongText("Or else ill have to get a new one", DialogLayout.Bottom)
+    }
+    if (controller.B.isPressed()) {
+        pause(3000)
+        rock = sprites.createProjectileFromSprite(assets.image`rock`, spy, 30, 30)
+        rock = sprites.createProjectileFromSprite(assets.image`rock`, spy_buddy, 30, 30)
+    }
 })
