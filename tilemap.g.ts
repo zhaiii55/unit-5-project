e6dec32b550f0a402c85f491ea75221696d91239
zhaiii55 <156235611+zhaiii55@users.noreply.github.com>
2024-01-23 12:46:55 -0600

    Restore '1/23/2024, 12:29:48 PM Update tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts'

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,6 +1,8 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
@@ -46,6 +48,8 @@ namespace myTiles {
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "myTile":
+            case "tile1":return tile1;
             case "transparency16":return transparency16;
         }
         return null;
